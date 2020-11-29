a16d16863774a4f5c69f4e426f7045752651da08
hopegipson <gipson.hope@gmail.com>
2020-11-28 20:34:50 -0600

    formatting of the page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,13 +2,23 @@ class TeamsController < ApplicationController
     before_action :find_team, only: [:show, :create]
 
     def index
-       @teams = Team.all
        current_user
+       if params[:user_id]
+        @teams = User.find(params[:user_id]).teams
+      else
+        @teams = Team.all
+      end
     end
       
     def new
-       @team = Team.new
-       @users = User.all
+      # @team = Team.new
+       #@users = User.all
+       if params[:user_id] && !User.exists?(params[:user_id])
+        redirect_to users_path, alert: "User not found."
+      else
+        @team = Team.new(user_id: params[:user_id])
+      end
+       
     end
 
     def create 
@@ -16,11 +26,18 @@ class TeamsController < ApplicationController
         @team.user_id = current_user.id
         return redirect_to new_team_path unless @team.save
         redirect_to team_path(@team)
-
+       
     end
 
     def show
-        current_user
+        if params[:user_id]
+            @user = User.find_by(id: params[:user_id])
+            @team = @User.teams.find_by(id: params[:id])
+
+            #@team = User.find(params[:user_id]).teams.find(params[:id])
+          else
+            @team = Team.find(params[:id])
+          end
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'application#home'
   resources :players, only: [:index, :show]
+  resources :users, only: [:index, :new, :show] do
+    resources :teams, only: [:show, :index, :new]
+  end
   resources :teams
-  resources :users
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy', as: 'logout'
   get '/search' => 'search#index', :as => 'search'
-
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
