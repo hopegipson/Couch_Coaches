a7c1038f030ea7f8ebc13ec5dfed0b6a7ecc345f
hopegipson <gipson.hope@gmail.com>
2020-12-03 22:45:24 -0600

    more cleanup in services views

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # README
-
+THIS IS THE RIGHT ONE
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/services/sports_data.rb
+++ app/services/sports_data.rb
@@ -1,15 +1,11 @@
 module SportsData
-    class Search
-
-      def self.by_player(player_name)
-        @player_name = player_name
-        response = Faraday.get 'https://api.sportsdata.io/v3/nfl/projections/json/PlayerSeasonProjectionStats/2020REG?key=' + ENV['API_KEY']
-        response =JSON.parse(response.body)
-        foundplayer = response.detect {|result| result["Name"].to_s.downcase == @player_name.downcase}
-        foundplayer
-      end
-
-
+  class Search
+    def self.by_player(player_name)
+      @player_name = player_name
+      response = Faraday.get 'https://api.sportsdata.io/v3/nfl/projections/json/PlayerSeasonProjectionStats/2020REG?key=' + ENV['API_KEY']
+      response =JSON.parse(response.body)
+      foundplayer = response.detect {|result| result["Name"].to_s.downcase == @player_name.downcase}
+      foundplayer
     end
-
   end
+end
